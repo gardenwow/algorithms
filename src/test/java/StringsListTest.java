d4386e59524ccee13ac9e83f0650eb59e955d17a
dreamworldsn <107345428+gardenwow@users.noreply.github.com>
2022-10-15 02:29:11 +0400

    1

---
+++ src/test/java/StringsListTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class StringsListTest {
+    private final StringsListTest stringsListTest = new StringsListTest();
+    @Test
+    private void addStringListTest(){
+        stringsListTest = {}
+
+    }
+}
